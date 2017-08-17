450b12c4764aa8dd20d7cb49dbdf4b2320dc184f
liuxiaoguang <xiaogliu@outlook.com>
2017-08-17 20:19:45 +0800

    跨浏览器事件对象

---
+++ JavaScript/method/跨浏览器事件对象.js
@@ -0,0 +1,41 @@
+// 定义跨浏览器事件对象
+var EventUtil = {
+  addHandler: function(element, type, handler){
+    if (element.addEventListener){
+      element.addEventListener(type, handler, false);
+    } else if (element.attachEvent){
+      element.attachEvent("on" + type, handler);
+    } else {
+      element["on" + type] = handler;
+    }
+  },
+  getEvent: function(event){
+    return event ? event : window.event;
+  },
+  getTarget: function(event){
+    return event.target || event.srcElement;
+  },
+  preventDefault: function(event){
+    if (event.preventDefault){
+      event.preventDefault();
+    } else {
+      event.returnValue = false;
+    }
+  },
+  removeHandler: function(element, type, handler){
+    if (element.removeEventListener){
+      element.removeEventListener(type, handler, false);
+    } else if (element.detachEvent){
+      element.detachEvent("on" + type, handler);
+    } else {
+      element["on" + type] = null;
+    }
+  },
+  stopPropagation: function(event){
+    if (event.stopPropagation){
+      event.stopPropagation();
+    } else {
+      event.cancelBubble = true;
+    }
+  }
+};
