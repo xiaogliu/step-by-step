cd7fc4b3e98009d1f23b7ada136f832c773991ff
liuxiaoguang <vincexgliu@gmail.com>
2018-03-12 17:01:08 +0800

    添加路由文件

--- 05_node/1_node_static_server/server.js
+++ 05_node/1_node_static_server/server.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const path = require("path");
+const fs = require("fs");
 const config = require("./config/default.json");
 
 // 创建静态类
@@ -10,17 +11,40 @@ class StaticServer {
     this.indexPage = config.indexPage;
   }
 
+  respondNotFound(req, res) {
+    res.writeHead(404, {
+      "Content-Type": "text/html"
+    });
+    res.end(
+      `<h1>Not Found</h1><p>The requested URL ${
+        req.url
+      } was not found on this server.</p>`
+    );
+  }
+
+  respondFile(pathName, req, res) {
+    const readStream = fs.createReadStream(pathName);
+    readStream.pipe(res);
+  }
+
+  routeHandler(pathName, req, res) {
+    fs.stat(pathName, (err, stat) => {
+      if (!err) {
+        this.respondFile(pathName, req, res);
+      } else {
+        this.respondNotFound(req, res);
+      }
+    });
+  }
+
   start() {
     // 通过 http 模块创建服务器，设置监听端口
     http
       .createServer((req, res) => {
         const pathName = path.join(this.root, path.normalize(req.url));
 
-        // 发送 http 头部
-        res.writeHead(200);
-
-        // 发送响应数据
-        res.end(`Request path: ${pathName}`);
+        // 处理路由
+        this.routeHandler(pathName, req, res);
       })
       .listen(this.port, err => {
         if (err) {
