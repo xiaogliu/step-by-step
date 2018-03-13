fd546238c09353bc4746a3892c0d418a3e4f83d8
liuxiaoguang <vincexgliu@gmail.com>
2018-03-13 12:24:08 +0800

    设置文件压缩

--- 05_node/1_node_static_server/server.js
+++ 05_node/1_node_static_server/server.js
@@ -2,6 +2,7 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 const url = require("url");
+const zlib = require("zlib");
 const mime = require("./mime");
 const config = require("./config/default.json");
 
@@ -18,19 +19,68 @@ class StaticServer {
     this.enableETag = config.etag;
     this.enableLastModified = config.lastModified;
     this.maxAge = config.maxAge;
+    this.zipMatch = new RegExp(config.zipMatch);
   }
 
-  // 响应入口
-  respond(pathName, req, res) {
-    fs.stat(pathName, (err, stat) => {
-      if (err) return respondError(err, res);
-      this.setFreshHeaders(stat, res);
-      if (this.isFresh(req.headers, res._headers)) {
-        this.respondNotModified(res);
-      } else {
-        this.respondFile(pathName, req, res);
-      }
-    });
+  // 压缩文件
+  compressHandler(readStream, req, res) {
+    const acceptEncoding = req.headers["accept-encoding"];
+    if (!acceptEncoding || !acceptEncoding.match(/\b(gzip|deflate)\b/)) {
+      return readStream;
+    } else if (acceptEncoding.match(/\bgzip\b/)) {
+      res.setHeader("Content-Encoding", "gzip");
+      return readStream.pipe(zlib.createGzip());
+    } else if (acceptEncoding.match(/\bdeflate\b/)) {
+      res.setHeader("Content-Encoding", "deflate");
+      return readStream.pipe(zlib.createDeflate());
+    }
+  }
+
+  // 压缩文件
+  shouldCompress(pathName) {
+    return path.extname(pathName).match(this.zipMatch);
+  }
+
+  // 获取有效范围
+  getRange(rangeText, totalSize) {
+    const matchResults = rangeText.match(/bytes=([0-9]*)-([0-9]*)/);
+    let start = parseInt(matchResults[1]);
+    let end = parseInt(matchResults[2]);
+    if (isNaN(start) && !isNaN(end)) {
+      start = totalSize - end;
+      end = totalSize - 1;
+    } else if (!isNaN(start) && isNaN(end)) {
+      end = totalSize - 1;
+    }
+    return {
+      start,
+      end
+    };
+  }
+
+  // 接受范围请求，嘿嘿嘿片里面经常看到，范围有效返回206，不然返回416
+  rangeHandler(pathName, rangeText, totalSize, res) {
+    const range = this.getRange(rangeText, totalSize);
+    if (
+      range.start > totalSize ||
+      range.end > totalSize ||
+      range.start > range.end
+    ) {
+      res.statusCode = 416;
+      res.setHeader("Content-Range", `bytes */${totalSize}`);
+      res.end();
+      return null;
+    } else {
+      res.statusCode = 206;
+      res.setHeader(
+        "Content-Range",
+        `bytes ${range.start}-${range.end}/${totalSize}`
+      );
+      return fs.createReadStream(pathName, {
+        start: range.start,
+        end: range.end
+      });
+    }
   }
 
   // 产生 ETag
@@ -96,11 +146,24 @@ class StaticServer {
   }
 
   // 响应文件
-  respondFile(pathName, req, res) {
-    const readStream = fs.createReadStream(pathName);
-
-    // 设置mime
+  respondFile(stat, pathName, req, res) {
+    let readStream;
     res.setHeader("Content-Type", mime.lookup(pathName));
+    res.setHeader("Accept-Ranges", "bytes");
+    if (req.headers["range"]) {
+      readStream = this.rangeHandler(
+        pathName,
+        req.headers["range"],
+        stat.size,
+        res
+      );
+      if (!readStream) return;
+    } else {
+      readStream = fs.createReadStream(pathName);
+    }
+    if (this.shouldCompress(pathName)) {
+      readStream = this.compressHandler(readStream, req, res);
+    }
     readStream.pipe(res);
   }
 
@@ -108,12 +171,11 @@ class StaticServer {
   respondDirectory(pathName, req, res) {
     const indexPagePath = path.join(pathName, this.indexPage);
     if (fs.existsSync(indexPagePath)) {
-      this.respondFile(indexPagePath, req, res);
+      this.respond(indexPagePath, req, res);
     } else {
       fs.readdir(pathName, (err, files) => {
         if (err) {
-          res.writeHead(500);
-          return res.end(err);
+          respondError(err, res);
         }
         const requestPath = url.parse(req.url).pathname;
         let content = `<h1>Index of ${requestPath}</h1>`;
@@ -141,6 +203,19 @@ class StaticServer {
       "Content-Type": "text/html"
     });
     res.end(`Redirecting to <a href='${location}'>${location}</a>`);
+  }
+
+  // 响应入口
+  respond(pathName, req, res) {
+    fs.stat(pathName, (err, stat) => {
+      if (err) return respondError(err, res);
+      this.setFreshHeaders(stat, res);
+      if (this.isFresh(req.headers, res._headers)) {
+        this.respondNotModified(res);
+      } else {
+        this.respondFile(stat, pathName, req, res);
+      }
+    });
   }
 
   // 路由处理
