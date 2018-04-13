50181c0b60e4ec0f35891bfa61742360e242aafe
liuxiaoguang <vincexgliu@gmail.com>
2018-04-13 12:09:55 +0800

    引入惰性载入

--- 09_Utilties/2_ui_component/02_fullPageScroll/js/components/utils.js
+++ 09_Utilties/2_ui_component/02_fullPageScroll/js/components/utils.js
@@ -1,5 +1,6 @@
 /**
- * 原生 js 语法，DOM 相关
+ * 工具函数
+ * 惰性载入函数降低了代码的可读性，如果不是频繁操作的项目，不引入惰性载入
  */
 export default {
   // 删除 类名
@@ -37,32 +38,70 @@ export default {
    */
   addHandler(element, type, handler) {
     if (element.addEventListener) {
-      // 好奇怪，放到类中如果惰性载入失败
+      // 第一次调用初始化
       element.addEventListener(type, handler, false);
+
+      // 第一次之后调用直接使用更改的函数，无返回值，不能使用箭头函数
+      this.addHandler = function(element, type, handler) {
+        element.addEventListener(type, handler, false);
+      };
     } else if (element.attachEvent) {
       element.attachEvent(`on${type}`, handler);
+
+      this.addHandler = function(element, type, handler) {
+        element.attachEvent(`on${type}`, handler);
+      };
     } else {
       element[`on${type}`] = handler;
+
+      this.addHandler = function(element, type, handler) {
+        element[`on${type}`] = handler;
+      };
     }
   },
   removeHandler(element, type, handler) {
     if (element.removeEventListener) {
       element.removeEventListener(type, handler, false);
+
+      this.removeHandler = function(element, type, handler) {
+        element.removeEventListener(type, handler, false);
+      };
     } else if (element.detachEvent) {
       element.detachEvent(`on${type}`, handler);
+
+      this.removeHandler = function(element, type, handler) {
+        element.detachEvent(`on${type}`, handler);
+      };
     } else {
       element[`on${type}`] = null;
+      this.removeHandler = function(element, type, handler) {
+        element[`on${type}`] = null;
+      };
     }
   },
   getEvent(event) {
-    return event ? event : window.event;
+    if (event) {
+      // 第一次调用之后惰性载入
+      this.getEvent = event => event;
+
+      // 第一次调用使用
+      return event;
+    } else {
+      this.getEvent = () => window.event;
+      return window.event;
+    }
   },
 
   // 鼠标滚轮事件
   getWheelDelta(event) {
     if (event.wheelDelta) {
+      // 第一次调用之后惰性载入
+      this.getWheelDelta = event => event.wheelDelta;
+
+      // 第一次调用使用
       return event.wheelDelta;
     } else {
+      this.getWheelDelta = event => -event.detail * 40;
       return -event.detail * 40;
     }
   },
